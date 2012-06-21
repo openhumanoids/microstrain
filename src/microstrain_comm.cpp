1e1306a708895a4dacc084d4c0e9186f0c3674b8
jskelly <jskelly@d41ffffc-f2b2-4b44-b683-edcb61ded693>
2012-06-21 15:34:19 +0000

    Added support for 500 Hz, 1000 Hz modes, setting digital filter window size.

--- src/microstrain_comm.cpp
+++ src/microstrain_comm.cpp
@@ -2,6 +2,7 @@
 #include <fcntl.h>   // File control definitions
 #include <errno.h>   // Error number definitions
 #include <assert.h>
+#include <signal.h>
 #include <sys/ioctl.h>
 
 #include <iostream>
@@ -28,10 +29,33 @@
 #define ACC_ANG_MAG_ROT 0xCC
 #define LENGTH_ACC_ANG_MAG_ROT 79
 
-#define CONTINUOUS_MODE_COMMAND 0xC4
+#define CONTINUOUS_MODE_COMMAND  0xC4
+#define LENGTH_CONTINUOUS_MODE      4
 #define LENGTH_CONTINUOUS_MODE_ECHO 8
 
-#define DELTA_ANG_VEL_DT 0.01
+#define COMMS_SETTINGS_COMMAND   0xD9
+#define LENGTH_COMMS_SETTINGS      11
+#define LENGTH_COMMS_SETTINGS_ECHO 10
+
+#define SAMPLING_SETTINGS_COMMAND   0xDB
+#define LENGTH_SAMPLING_SETTINGS      20
+#define LENGTH_SAMPLING_SETTINGS_ECHO 19
+
+#define DATA_RATE_DEFAULT  100
+#define DATA_RATE_MED      500
+#define DATA_RATE_HIGH    1000
+
+#define BAUD_RATE_DEFAULT 115200
+#define BAUD_RATE_MED     460800
+#define BAUD_RATE_HIGH    921600
+
+#define DELTA_ANG_VEL_DT_DEFAULT 0.010
+#define DELTA_ANG_VEL_DT_MED     0.002
+#define DELTA_ANG_VEL_DT_HIGH    0.001
+
+#define FILTER_WINDOW_SIZE_DEFAULT 15
+#define FILTER_WINDOW_SIZE_MIN      1
+#define FILTER_WINDOW_SIZE_MAX     32
 
 #define GRAVITY 9.80665
 
@@ -40,24 +64,40 @@ using namespace std;
 
 #define INPUT_BUFFER_SIZE 2000
 
+// global reference to Glib main loop
+static GMainLoop* mainloop = NULL;
+
 //self structure
 class app_t {
 public:
-  //communication variables
+  // communication variables
   int comm, status;
+  char comm_port_name[255];
+  unsigned int baud_rate, data_rate;
 
-  //input buffer variables
+  // dt for angular rate and acceleration computation
+  double delta_t;
+
+  // window size for gyro and accelerometer digital filter
+  int filter_window_size;
+
+  // input buffer variables
   Byte input_buffer[INPUT_BUFFER_SIZE];
   Byte message_mode;
   BotRingBuf * read_buffer;
   char current_segment;
   int expected_segment_length;
 
-  //packet variables
+  // packet variables
   int message_size;
   int message_start_byte;
 
-  //boolean setting flags
+  // state flags (not currently used, but may be in future)
+  bool changed_baud_rate;
+  bool changed_data_rate;
+  bool in_continuous_mode;
+
+  // boolean setting flags
   bool quiet, verbose;
 
   bool little_endian;
@@ -71,7 +111,6 @@ public:
 
   bot_timestamp_sync_state * sync;
   bool do_sync;
-
 };
 
 bool systemLittleEndianCheck()
@@ -86,7 +125,6 @@ bool systemLittleEndianCheck()
 
 unsigned int make32UnsignedInt(Byte* pBytes, bool little_endian)
 {
-
   unsigned int i;
 
   if (little_endian) {
@@ -107,7 +145,6 @@ unsigned int make32UnsignedInt(Byte* pBytes, bool little_endian)
 
 unsigned int make16UnsignedInt(const Byte* pBytes, bool little_endian)
 {
-
   unsigned int i;
 
   if (little_endian) {
@@ -121,6 +158,20 @@ unsigned int make16UnsignedInt(const Byte* pBytes, bool little_endian)
   }
 
   return i;
+}
+
+void makeUnsignedInt16(unsigned int val, Byte* high, Byte* low)
+{
+  *low  = static_cast<Byte>(val);
+  *high = static_cast<Byte>(val >> 8);
+}
+
+void makeUnsignedInt32(unsigned int val, Byte* byte3, Byte* byte2, Byte* byte1, Byte* byte0)
+{
+  *byte0 = static_cast<Byte>(val);
+  *byte1 = static_cast<Byte>(val >>  8);
+  *byte2 = static_cast<Byte>(val >> 16);
+  *byte3 = static_cast<Byte>(val >> 24);
 }
 
 float make32bitFloat(const Byte* pBytes, bool little_endian)
@@ -158,6 +209,27 @@ void convertFloatToDouble(double * dest, const float * source, int length)
   for (int ii = 0; ii < length; ii++) {
     dest[ii] = (double) source[ii];
   }
+}
+
+static void sig_action(int signal, siginfo_t *s, void *user)
+{
+  // kill the glib main loop...
+  if (g_main_loop_is_running(mainloop)) {
+    g_main_loop_quit(mainloop);
+  }
+}
+
+void install_signal_handler()
+{
+  struct sigaction action;
+  action.sa_sigaction = sig_action;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = 0;
+
+  sigaction(SIGINT,  &action, NULL);
+  sigaction(SIGTERM, &action, NULL);
+  sigaction(SIGKILL, &action, NULL);
+  sigaction(SIGHUP,  &action, NULL);
 }
 
 // ------------- Functions taken from microstrain driver for opening commport with proper settings and finding an attached microstrain device -----------
@@ -220,27 +292,14 @@ bool scandev(char * comm_port_name)
 
 }
 
-// OpenComPort
-// Opens a com port with the correct settings for communicating with a MicroStrain 3DM-GX3-25 sensor
-
-int OpenComPort(const char* comPortPath)
+int setup_com_port(int comPort, speed_t baudRate)
 {
-
-  int comPort = open(comPortPath, O_RDWR | O_NOCTTY);
-
-  if (comPort == -1) { //Opening of port failed
-
-    printf("Unable to open com Port %s\n Errno = %i\n", comPortPath, errno);
-    return -1;
-
-  }
-
   //Get the current options for the port...
   struct termios options;
   tcgetattr(comPort, &options);
 
-  //set the baud rate to 115200
-  int baudRate = B115200;
+  // set the desired baud rate (default for MicroStrain is 115200)
+  // int baudRate = B115200;
   cfsetospeed(&options, baudRate);
   cfsetispeed(&options, baudRate);
 
@@ -282,7 +341,22 @@ int OpenComPort(const char* comPortPath)
   tcflush(comPort, TCIOFLUSH);
 
   return comPort;
+}
 
+// Opens a com port with the correct settings for communicating with a MicroStrain 3DM-GX3-25 sensor
+// Tweaked - split into two, added baud rate argument
+int open_com_port(const char* comPortPath, speed_t baudRate)
+{
+  int comPort = open(comPortPath, O_RDWR | O_NOCTTY);
+
+  if (comPort == -1) { //Opening of port failed
+
+    printf("Unable to open com Port %s\n Errno = %i\n", comPortPath, errno);
+    return -1;
+
+  }
+
+  return setup_com_port(comPort, baudRate);
 }
 
 /*
@@ -309,9 +383,137 @@ unsigned short cksum(const Byte * packet_bytes, int packet_length)
   return check_sum_val;
 }
 
+/*
+ * sets continuous streaming operation
+ */
+bool set_continuous_mode(app_t* app)
+{
+  char set_mode_string[] = { 0xC4, 0xC1, 0x29, app->message_mode };
+
+  cout << "Setting continuous mode" << endl;
+
+  if (write(app->comm, set_mode_string, LENGTH_CONTINUOUS_MODE) != LENGTH_CONTINUOUS_MODE) {
+    cerr << "Error writing command to set continuous mode" << endl;
+    return false;
+  }
+
+  return true;
+}
+
+/*
+ * stops continous streaming - command does not generate a response
+ */
+void stop_continuous_mode(app_t* app)
+{
+  char stop_mode_string[] = { 0xFA, 0x75, 0xB4 };
+
+  if (write(app->comm, stop_mode_string, 3) != 3) {
+    cerr << "Error writing command to stop continuous mode" << endl;
+  }
+}
+
+/*
+ * soft device reset - return to default settings
+ */
+void soft_reset(app_t* app)
+{
+  char soft_reset_string[] = { 0xFE, 0x9E, 0x3A };
+
+  if (write(app->comm, soft_reset_string, 3) != 3) {
+    cerr << "Error writing command to stop continuous mode" << endl;
+  }
+}
+
+/*
+ * sets/changes comms baud rate
+ */
+bool set_comms_baud_rate(app_t* app)
+{
+  Byte baud0, baud1, baud2, baud3;
+
+  makeUnsignedInt32(app->baud_rate, &baud3, &baud2, &baud1, &baud0);
+
+  char set_comms_baud_rate_string[] =
+    { COMMS_SETTINGS_COMMAND, // Byte  1  : command
+      0xC3,                   // Bytes 2-3: confirm intent
+      0x55,
+      0x01,                   // Byte  4  : port selector
+      0x01,                   // Byte  5  : temporary change
+      baud3,                  // Bytes 6-9: baud rate
+      baud2,
+      baud1,
+      baud0,
+      0x02,                   // Byte  10 : port config
+      0x00                    // Byte  11 : reserved (zero)
+    };
+
+  if (app->verbose)
+    cout << "Setting baud rate" << endl;
+
+  if (write(app->comm, set_comms_baud_rate_string, LENGTH_COMMS_SETTINGS) != LENGTH_COMMS_SETTINGS) {
+    cerr << "Error writing command to set comms baud rate" << endl;
+    return false;
+  }
+
+  return true;
+}
+
+/*
+ * sets/changes sensor update (data) rate and filter window size
+ */
+bool set_sampling_settings(app_t* app)
+{
+  Byte ocsb;       // orientation, coning & sculling byte
+  Byte decu, decl; // decimation value upper & lower bytes
+  Byte wndb;       // digital filter window size byte
+
+  unsigned int decimation = 1000/app->data_rate;
+  makeUnsignedInt16(decimation, &decu, &decl);
+
+  // don't compute orientation if we're running at max rate
+  if (app->data_rate == DATA_RATE_HIGH)
+    ocsb = 0x02;
+  else
+    ocsb = 0x03;
+
+  wndb = static_cast<Byte>(app->filter_window_size);
+
+  char set_sampling_params_string[] =
+    { SAMPLING_SETTINGS_COMMAND, // Byte  1    : command
+      0xA8,                      // Bytes 2-3  : confirm intent
+      0xB9,
+      0x01,                      // Byte  4    : change params
+      decu,                      // Bytes 5-6  : decimation value
+      decl,
+      0x00,                      // Bytes 7-8  : flags - orient
+      ocsb,
+      wndb,                      // Byte  9    : gyro/accel window size
+      0x11,                      // Byte  10   : magneto window size
+      0x00,                      // Byte  11-12: up compensation
+      0xA0,
+      0x00,                      // Byte  13-14: north compensation
+      0xA0,
+      0x01,                      // Byte  15   : low magneto power
+      0x00,                      // Bytes 16-20: reserved (zeros)
+      0x00,
+      0x00,
+      0x00,
+      0x00
+     };
+
+  if (app->verbose)
+    cout << "Setting sampling settings" << endl;
+
+  if (write(app->comm, set_sampling_params_string, LENGTH_SAMPLING_SETTINGS) != LENGTH_SAMPLING_SETTINGS) {
+    cerr << "Error writing command to set sampling settings" << endl;
+    return false;
+  }
+
+  return true;
+}
+
 bool handle_message(app_t* app)
 {
-
   microstrain_ins_t ins_message;
   memset(&ins_message, 0, sizeof(ins_message));
   int ins_timer;
@@ -325,7 +527,8 @@ bool handle_message(app_t* app)
   }
 
   bool got_quat = false;
-  int success = 0;
+  bool success = true;
+
   switch (app->message_start_byte) {
   case ACC_ANG_MAG_ROT:
     {
@@ -419,11 +622,11 @@ bool handle_message(app_t* app)
       //get the data we care about
       unpack32BitFloats(vals, &app->input_buffer[1], 3, app->little_endian);
       convertFloatToDouble(ins_message.gyro, vals, 3);
-      bot_vector_scale_3d(ins_message.gyro, 1 / DELTA_ANG_VEL_DT);
+      bot_vector_scale_3d(ins_message.gyro, 1 / app->delta_t);
 
       unpack32BitFloats(vals, &app->input_buffer[13], 3, app->little_endian);
       convertFloatToDouble(ins_message.accel, vals, 3);
-      bot_vector_scale_3d(ins_message.accel, 1 / DELTA_ANG_VEL_DT);
+      bot_vector_scale_3d(ins_message.accel, 1 / app->delta_t);
       bot_vector_scale_3d(ins_message.accel, GRAVITY);
 
       unpack32BitFloats(vals, &app->input_buffer[25], 3, app->little_endian);
@@ -445,17 +648,46 @@ bool handle_message(app_t* app)
     }
   case CONTINUOUS_MODE_COMMAND:
     {
-      fprintf(stderr, "received continuous mode command echo\n");
+      fprintf(stderr, "Received continuous mode command echo\n");
+      app->in_continuous_mode = true;
+      break;
+    }
+  case SAMPLING_SETTINGS_COMMAND:
+    {
+      if (app->verbose)
+        fprintf(stderr, "Recieved sampling settings command echo\n");
+
+      if (app->data_rate != DATA_RATE_DEFAULT) {
+        app->changed_data_rate = true;
+      }
+
+      success = set_continuous_mode(app);
+      break;
+    }
+  case COMMS_SETTINGS_COMMAND:
+    {
+      if (app->verbose)
+        fprintf(stderr, "Recieved comms baud rate command echo\n");
+
+      // received echo at the current baud rate, now switch to desired baud rate
+      if(setup_com_port(app->comm, app->baud_rate) < 0) {
+        success = false;
+      }
+      else {
+        app->changed_baud_rate = true;
+        success = set_sampling_settings(app);
+      }
       break;
     }
   default:
     {
       if (!app->quiet)
         fprintf(stderr, "Unknown message start byte: %d\n", app->message_start_byte);
+      break;
     }
   }
 
-  return true;
+  return success;
 }
 
 /*
@@ -466,7 +698,6 @@ bool handle_message(app_t* app)
  */
 void unpack_packets(app_t * app)
 {
-
   while (bot_ringbuf_available(app->read_buffer) >= app->expected_segment_length) {
     switch (app->current_segment) {
     case 's':
@@ -493,6 +724,12 @@ void unpack_packets(app_t * app)
       case CONTINUOUS_MODE_COMMAND:
         app->expected_segment_length = LENGTH_CONTINUOUS_MODE_ECHO;
         break;
+      case SAMPLING_SETTINGS_COMMAND:
+        app->expected_segment_length = LENGTH_SAMPLING_SETTINGS_ECHO;
+        break;
+      case COMMS_SETTINGS_COMMAND:
+        app->expected_segment_length = LENGTH_COMMS_SETTINGS_ECHO;
+        break;
       default:
         if (!app->quiet) {
           fprintf(stderr, "no match for message start byte %d\n", app->message_start_byte);
@@ -500,6 +737,7 @@ void unpack_packets(app_t * app)
         //read a byte and continue if we don't have a match
         bot_ringbuf_read(app->read_buffer, 1, (uint8_t *) &app->message_start_byte);
         app->current_segment = 's';
+        break;
       }
       break;
     case 'p':
@@ -514,7 +752,7 @@ void unpack_packets(app_t * app)
       }
 
       if (app->verbose)
-        fprintf(stderr, "Passed checksum,handling message\n");
+        fprintf(stderr, "Passed checksum, handling message\n");
 
       bool message_success = handle_message(app);
       if (!message_success && !app->quiet)
@@ -571,23 +809,29 @@ static gboolean serial_read_handler(GIOChannel * source, GIOCondition condition,
   return TRUE;
 }
 
-
 int main(int argc, char **argv)
 {
-
   app_t * app = new app_t();
   app->little_endian = systemLittleEndianCheck();
 
-  //default settings
+  // default settings
   app->verbose = 0;
   app->quiet = 0;
   app->message_mode = DANG_DVEL_MAG;
+  app->data_rate = DATA_RATE_DEFAULT;
+  app->baud_rate = BAUD_RATE_DEFAULT;
+  app->delta_t = DELTA_ANG_VEL_DT_DEFAULT;
+  app->filter_window_size = FILTER_WINDOW_SIZE_DEFAULT;
   app->do_sync = true;
   app->channel = "MICROSTRAIN_INS";
 
-  bool auto_comm = true;
+  app->changed_baud_rate = false;
+  app->changed_data_rate = false;
+  app->in_continuous_mode = false;
 
   string user_comm_port_name;
+  bool auto_comm = true;
+  char data_rate = 'l';
 
   bool acc_ang_mag_rot = false;
   bool acc_ang_mag = false;
@@ -596,30 +840,80 @@ int main(int argc, char **argv)
   ConciseArgs opt(argc, argv);
   opt.add(app->verbose, "v", "verbose");
   opt.add(app->quiet, "q", "quiet");
-  opt.add(user_comm_port_name, "d", "dev");
-
+  opt.add(user_comm_port_name, "d", "dev", "Device file to connect to (default is automatic scan)");
+  opt.add(data_rate, "b", "rate", "Update rate: 'l'-low (100 Hz), 'm'-medium (500 Hz), 'h'-high (1000 Hz)");
+  opt.add(app->filter_window_size, "w", "window", "Gyro & accel digital filter window size: 1-32");
   opt.add(acc_ang_mag_rot, "r", "quat");
   opt.add(acc_ang_mag, "n", "no_delta");
   opt.add(acc_stab, "f", "filter");
-  opt.add(app->channel, "c", "channel");
-
+  opt.add(app->channel, "c", "channel", "LCM message output channel");
   opt.add(app->do_sync, "s", "time_sync");
-
   opt.parse();
 
-  if (acc_ang_mag_rot)
-    app->message_mode = ACC_ANG_MAG_ROT;
-  if (acc_ang_mag)
-    app->message_mode = ACC_ANG_MAG;
-  if (acc_stab)
-    app->message_mode = ACC_STAB;
   if (opt.wasParsed("dev"))
     auto_comm = false;
+
+  // data rate (which also determines baud rate)
+  if (opt.wasParsed("rate")) {
+    switch (data_rate) {
+    case 'l':
+      // Default
+      break;
+    case 'm':
+      app->data_rate = DATA_RATE_MED;
+      app->baud_rate = BAUD_RATE_MED;
+      app->delta_t = DELTA_ANG_VEL_DT_MED;
+      break;
+    case 'h':
+      app->data_rate = DATA_RATE_HIGH;
+      app->baud_rate = BAUD_RATE_HIGH;
+      app->delta_t = DELTA_ANG_VEL_DT_HIGH;
+      break;
+    default:
+      cerr << "Unknown update rate flag - using default rate" << endl;
+      break;
+    }
+  }
+
+  if(!app->quiet)
+    cout << "Setting data rate to " << app->data_rate << " Hz" << endl;
+
+  // make sure filter window size isn't too big or small
+  if (opt.wasParsed("window")) {
+    if (app->filter_window_size < FILTER_WINDOW_SIZE_MIN) {
+      app->filter_window_size = FILTER_WINDOW_SIZE_DEFAULT;
+      cerr << "Digital filter window size too small, using default size" << endl;
+    }
+    else if (app->filter_window_size > FILTER_WINDOW_SIZE_MAX) {
+      app->filter_window_size = FILTER_WINDOW_SIZE_DEFAULT;
+      cerr << "Digital filter window size too large, using default size" << endl;
+    }
+
+    if (!app->quiet)
+      cout << "Setting digital filter window size to " << app->filter_window_size << endl;
+  }
+
+  // modes are mutually exlusive
+  if (acc_stab) {
+    app->message_mode = ACC_STAB;
+  }
+  else if (acc_ang_mag) {
+    app->message_mode = ACC_ANG_MAG;
+  }
+  else if (acc_ang_mag_rot) {
+    if(app->data_rate != DATA_RATE_HIGH) {
+      app->message_mode = ACC_ANG_MAG_ROT;
+    }
+    else {
+      cout << "Can't compute orientation at high speed, using delta angle + delta velocity mode instead" << endl;
+      app->message_mode = DANG_DVEL_MAG;
+    }
+  }
 
   if (!app->quiet)
     fprintf(stderr, "Little endian = %d\n", (int) app->little_endian);
 
-  GMainLoop * mainloop = g_main_loop_new(NULL, FALSE);
+  mainloop = g_main_loop_new(NULL, FALSE);
   app->lcm = bot_lcm_get_global(NULL);
   app->utime_prev = bot_timestamp_now();
   app->sync = bot_timestamp_sync_init(62500, (int64_t) 68719 * 62500, 1.001);
@@ -627,24 +921,38 @@ int main(int argc, char **argv)
 
   app->read_buffer = bot_ringbuf_create(INPUT_BUFFER_SIZE);
 
-  char comm_port_name[255];
   if (auto_comm)
-    scandev(comm_port_name);
+    scandev(app->comm_port_name);
   else
-    strcpy(comm_port_name, user_comm_port_name.c_str());
+    strcpy(app->comm_port_name, user_comm_port_name.c_str());
 
-  // comm initialization
-  app->comm = OpenComPort(comm_port_name);
+  // initialize comm port at default baud rate
+  app->comm = open_com_port(app->comm_port_name, BAUD_RATE_DEFAULT);
   if (app->comm < 0) {
     exit(1);
   }
 
-  char set_mode_string[] = { 0xC4, 0xC1, 0x29, app->message_mode };
-  cout << "setting continuous mode\n";
-  int written = write(app->comm, set_mode_string, 4);
-  if (written != 4) {
-    printf("Error writing command to set continuous mode");
-    exit(1);
+  // install signal handler
+  install_signal_handler();
+
+  // simple state machine
+  if (app->data_rate == DATA_RATE_DEFAULT && app->filter_window_size == FILTER_WINDOW_SIZE_DEFAULT) {
+    // set continous mode and we're done
+    if (!set_continuous_mode(app)) {
+      exit(1);
+    }
+  }
+  else if (app->data_rate == DATA_RATE_DEFAULT) {
+    // set filter window size, then set continous mode
+    if(!set_sampling_settings(app)) {
+      exit(1);
+    }
+  }
+  else {
+    // set baud rate, then sampling settings, then continuous mode
+    if(!set_comms_baud_rate(app)) {
+      exit(1);
+    }
   }
 
   app->current_segment = 's';
@@ -653,6 +961,10 @@ int main(int argc, char **argv)
   g_io_add_watch_full(ioc, G_PRIORITY_HIGH, G_IO_IN, (GIOFunc) serial_read_handler, (void *) app, NULL);
 
   g_main_loop_run(mainloop);
+
+  // received signal - soft reset to cleanup before quitting
+  soft_reset(app);
+
+  close(app->comm);
   return 0;
 }
-
